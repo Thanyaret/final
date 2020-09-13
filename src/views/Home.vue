c5a018b854e271cab01a051bac70cef08a9e3133
unknown <saikwon88@gmail.com>
2020-09-13 17:39:29 +0700

    updateproject

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+   <v-card
+    color="grey darken-2"
+    height="50px"
+  >   
+      <v-card-text >
+        <div class=" white--text " >ระบบเช็คจำนวน</div>
+      </v-card-text>
+      
+      <v-spacer></v-spacer>
+  </v-card>
+  <br>
+  <br>
+    <h1 class="text-center display-3 ">เดินทางมามหาวิทยาลัย 140 คน</h1><br>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs20 sm8 md8 lg3 v-for ="person in home" :key="person.name">
+        <v-card color="grey lighten-2" class=" ma-5">
+        <v-card-text>
+        <div class="headline">{{person.name}}</div><br>
+        <div class="subtitle">{{person.time}}</div><br>
+        <v-divider class="mx-4"></v-divider><br>
+        <div class="display-3 text-center">{{person.role}}</div><br>     
+        </v-card-text>
+        </v-card>    
+        </v-flex> 
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      home: [
+        {name: 'ตึก ICT',role:'35 คน',time:'Mon, 12:30 PM'},
+        {name: 'ตึก PKY',role:'35 คน',time:'Mon, 12:30 PM'},
+        {name: 'ตึก CE',role:'35 คน',time:'Mon, 12:30 PM'},
+        {name: 'ตึก EN',role:'35 คน',time:'Mon, 12:30 PM'},
+      ]
+    }
+  }
+
+}
+</script>
